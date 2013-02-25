39f27fdaa9c904b23a0770e8f4a49dbbac29558e
Hiroyuki Sano <sh19910711@gmail.com>
2013-02-25 18:21:41 +0900

    extgcd, modinvの実装

---
+++ math/modinv.cpp
@@ -0,0 +1,9 @@
+// @snip <sh19910711/contest:math/extgcd.cpp>
+template <class T> T modinv( T a, T m ) {
+    T x, y;
+    extgcd( a, m, x, y );
+    x %= m;
+    while ( x < 0 )
+        x += m;
+    return x;
+}
