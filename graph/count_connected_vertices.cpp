8af47456a1321af97cb9ae53a533d81c005ccd66
Hiroyuki Sano <sh19910711@gmail.com>
2013-02-27 06:02:23 +0900

    名前空間の修正

--- graph/count_connected_vertices.cpp
+++ graph/count_connected_vertices.cpp
@@ -1,15 +1,16 @@
+// @snip <sh19910711/contest:graph/adjacent_list.cpp>
 int CountConnectedVertices( graph::AdjacentListGraph& graph, int start_vertex ) {
     int n = graph.num;
     bool used[n];
-    fill( used, used+n, false );
-    queue <int> Q;
+    std::fill( used, used+n, false );
+    std::queue <int> Q;
     Q.push(start_vertex);
     used[start_vertex] = true;
     int res = 1;
     while ( ! Q.empty() ) {
         int current_vertex = Q.front();
         Q.pop();
-        for ( vector<int>::iterator it_i = graph.begin(current_vertex);
+        for ( std::vector<int>::iterator it_i = graph.begin(current_vertex);
                 it_i != graph.end(current_vertex); ++ it_i ) {
             int& next_vertex = *it_i;
             if ( used[next_vertex] )
