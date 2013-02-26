b3621f2c1ef8cc42c82dd48940693904839b5004
Hiroyuki Sano <sh19910711@gmail.com>
2013-02-26 23:59:32 +0900

    table/keymap.cppを追加。

---
+++ table/keymap.cpp
@@ -0,0 +1,18 @@
+template <class Key, class Data>
+class KeyMappingTable {
+public:
+    std::vector<Data> memory;
+    void init( Key& limit ) {
+        memory.resize(mapping(limit));
+    }
+    void fill( Data& value ) {
+        std::fill( memory.begin(), memory.end(), value );
+    }
+    virtual int mapping( Key& key ) = 0;
+    Data& operator []( Key& key ) {
+        return memory[mapping(key)];
+    }
+    Data& operator =( Data& right ) {
+        memory = right.memory;
+    }
+};
