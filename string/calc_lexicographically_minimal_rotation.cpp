9ad80b2028feb4eb5ddb4d5a1b50a1ea9ad1ba9e
Hiroyuki Sano <sh19910711@gmail.com>
2013-02-27 22:25:57 +0900

    辞書順で最小となる文字列の回転回数を求める関数の実装

---
+++ string/calc_lexicographically_minimal_rotation.cpp
@@ -0,0 +1,21 @@
+int CalcLexicographicallyMinimalRotation( const std::string& s ) {
+    int n = s.size();
+    int i = 0, j = 1, k = 0;
+    int x, y;
+    while ( i < n && j < n && k < n ) {
+        x = i + k >= n ? i + k - n : i + k;
+        y = j + k >= n ? j + k - n : j + k;
+        if ( s[x] == s[y] ) {
+            k ++;
+        } else {
+            if ( s[x] > s[y] )
+                i += k + 1;
+            else
+                j += k + 1;
+            if ( i == j )
+                j ++;
+            k = 0;
+        }
+    }
+    return i + 1;
+}
