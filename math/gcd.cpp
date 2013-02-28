bf80232b313efbe8ce637fe79c1594a8121e92c7
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-01 08:43:29 +0900

    min関数の呼び出し方をstd::minに修正

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -1,17 +1,11 @@
 template <class T> T gcd( T a, T b ) {
     T i, j;
-    for ( i = 0; ! ( a & 1 ); ++ i )
-        a >>= 1;
-    for ( j = 0; ! ( b & 1 ); ++ j )
-        b >>= 1;
+    for ( i = 0; ! ( a & 1 ); ++ i ) a >>= 1;
+    for ( j = 0; ! ( b & 1 ); ++ j ) b >>= 1;
     while ( b != a ) {
         b -= a;
-        if ( b < 0 ) {
-            a += b;
-            b = -b;
-        }
-        while ( ! ( b & 1 ) )
-            b >>= 1;
+        if ( b < 0 ) a += b, b = -b;
+        while ( ! ( b & 1 ) ) b >>= 1;
     }
-    return a << min( i, j );
+    return a << std::min( i, j );
 }
