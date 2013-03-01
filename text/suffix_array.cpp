e7a6133d4fc064d8e9bf38fd62b14df2c5418313
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-02 04:18:42 +0900

    コードを短縮した

--- text/suffix_array.cpp
+++ text/suffix_array.cpp
@@ -2,40 +2,27 @@ class SuffixArray {
 public:
     typedef std::vector<int> VI;
     std::string s;
-    int n;
-    int k;
-    VI sa;
-    VI rank;
-    VI height;
+    int n, k;
+    VI sa, rank, height;
     void build() {
         int maxlen = n;
-        for ( int i = 0; i < n; ++ i ) {
-            rank[i] = s[i];
-            maxlen = std::max( maxlen, s[i] + 1 );
-        }
+        for ( int i = 0; i < n; ++ i ) rank[i] = s[i], maxlen = std::max( maxlen, s[i] + 1 );
         VI cnt(maxlen, 0);
-        for ( int i = 0; i < n; ++ i )
-            cnt[rank[i]] ++;
-        for ( int i = 1; i < maxlen; ++ i )
-            cnt[i] += cnt[i-1];
-        for ( int i = 0; i < n; ++ i )
-            sa[--cnt[rank[i]]] = i;
-
+        for ( int i = 0; i < n; ++ i ) cnt[rank[i]] ++;
+        for ( int i = 1; i < maxlen; ++ i ) cnt[i] += cnt[i-1];
+        for ( int i = 0; i < n; ++ i ) sa[--cnt[rank[i]]] = i;
         for ( k = 1; k < n; k *= 2 ) {
             VI r = rank;
             rank[sa[0]] = 0;
             for ( int i = 1; i < n; ++ i ) {
-                int s1 = sa[i-1];
-                int s2 = sa[i];
+                int s1 = sa[i-1], s2 = sa[i];
                 rank[s2] = r[s1] == r[s2] && std::max( s1, s2 ) + k < n && r[s1+k/2] == r[s2+k/2] ? rank[s1] : i;
             }
-            for ( int i = 0; i < n; ++ i )
-                cnt[i] = i;
+            for ( int i = 0; i < n; ++ i ) cnt[i] = i;
             VI s = sa;
             for ( int i = 0; i < n; ++ i ) {
                 int s1 = s[i] - k;
-                if ( s1 >= 0 )
-                    sa[cnt[rank[s1]]++] = s1;
+                if ( s1 >= 0 ) sa[cnt[rank[s1]]++] = s1;
             }
         }
     }
@@ -46,11 +33,7 @@ public:
         init(s_);
     }
     void init( const std::string& s_ ) {
-        s = s_;
-        n = s.size();
-        sa.resize(n);
-        rank.resize(n+1);
-        height.resize(std::max(0,n-1));
+        s = s_; n = s.size(); sa.resize(n); rank.resize(n+1);height.resize(std::max(0,n-1));
     }
     VI::iterator begin() { return sa.begin(); }
     VI::iterator end() { return sa.end(); }
