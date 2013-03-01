946b242f752e55e1a947efc67b2bb7bbfe3f9f88
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-02 01:53:48 +0900

    トライ木の実装を追加

---
+++ table/trie.cpp
@@ -0,0 +1,27 @@
+template <class ValueType> class Trie {
+public:
+    typedef std::string::const_iterator StringIterator;
+    static const int SIZE = 256;
+    ValueType value;
+    Trie *next[SIZE];
+    Trie() { std::fill( next, next+SIZE, (Trie*)NULL ); }
+    Trie& find( const std::string& s ) {
+        Trie* res = this;
+        for ( StringIterator it_i = s.begin(); it_i != s.end(); ++ it_i ) {
+            const int& c = *it_i;
+            if ( ! res->next[c] ) res->next[c] = new Trie;
+            res = res->next[c];
+        }
+        return *res;
+    }
+    int count( const std::string& s ) {
+        Trie* res = this;
+        for ( StringIterator it_i = s.begin(); it_i != s.end(); ++ it_i ) {
+            const int& c = *it_i;
+            if ( ! res->next[c] ) return 0;
+            res = res->next[c];
+        }
+        return 1;
+    }
+    ValueType& operator []( const std::string& s ) { return find(s).value; }
+};
