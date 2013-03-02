bfc5112929855889c15bcea8cac822ea40bc96d6
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-03 01:42:39 +0900

    change function name: calc_fib -> CalcFib

--- math/calc_fib.cpp
+++ math/calc_fib.cpp
@@ -1,5 +1,5 @@
 // @snip<sh19910711/contest:math/matrix_2x2.cpp>
-template <class T, class U> T calc_fib( U n ) {
+template <class T, class U> T CalcFib( U n ) {
     if ( n == 0 ) return 0;
     if ( n == 1 ) return 1;
     Matrix2x2<T> mat( 1, 1, 1, 0 );
