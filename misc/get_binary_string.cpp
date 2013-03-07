d6216bb8ca405ec275c6a9c15c262fe4e60e408e
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-07 21:41:45 +0900

    与えられたデータを2進表記に変換する機能を実装した

---
+++ misc/get_binary_string.cpp
@@ -0,0 +1,10 @@
+template <class T> std::string GetBinaryString( const T& v ) {
+    std::ostringstream oss;
+    for ( int i = 0; i < static_cast<int>(sizeof v) * 8; ++ i ) {
+        int bi = ( v >> i ) & 1;
+        oss << bi;
+    }
+    std::string res = oss.str();
+    std::reverse( res.begin(), res.end() );
+    return res;
+}
