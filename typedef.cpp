db0745ace0555b2bd0840443733a59f3e0fd60e3
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-10 22:55:23 +0900

    typedefのテンプレートを追加

---
+++ typedef.cpp
@@ -0,0 +1,9 @@
+typedef istringstream ISS;
+typedef ostringstream OSS;
+typedef vector<std::string> VS;
+typedef long long LL;
+typedef int INT;
+typedef vector<INT> VI;
+typedef vector<VI> VVI;
+typedef pair<INT,INT> II;
+typedef vector<II> VII;
