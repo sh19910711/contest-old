087f1acd05a2bf7051d4c7bee70a4191975eb69c
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-10 23:23:50 +0900

    更新: typedef.cpp

--- solution/typedef.cpp
+++ solution/typedef.cpp
@@ -1,9 +1,9 @@
-typedef istringstream ISS;
-typedef ostringstream OSS;
-typedef vector<std::string> VS;
+typedef std::istringstream ISS;
+typedef std::ostringstream OSS;
+typedef std::vector<std::string> VS;
 typedef long long LL;
 typedef int INT;
-typedef vector<INT> VI;
-typedef vector<VI> VVI;
-typedef pair<INT,INT> II;
-typedef vector<II> VII;
+typedef std::vector<INT> VI;
+typedef std::vector<VI> VVI;
+typedef std::pair<INT,INT> II;
+typedef std::vector<II> VII;
