16db85fa657a5ef16ed98c5611db73099d45df06
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-11 17:01:38 +0900

    pair.cppを更新

--- io/pair.cpp
+++ io/pair.cpp
@@ -1,3 +1,6 @@
 template <class A, class B> std::ostream& operator<<( std::ostream& os, const std::pair<A,B>& p ) {
     return os << "(" << p.first << "," << p.second << ")";
 }
+template <class A, class B> std::istream& operator>>( std::istream& is, std::pair<A,B>& p ) {
+    return is >> p.first >> p.second;
+}
