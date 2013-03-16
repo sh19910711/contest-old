77fdf0ad83e5190c8fdccb942c7d0e34a9ed5cf4
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-16 23:18:44 +0900

    Rolling Hashのインターフェースを変更

--- hash/rolling_hash/interface.cpp
+++ hash/rolling_hash/interface.cpp
@@ -2,9 +2,5 @@
 template <class T> class IRollingHash {
 public:
     virtual void init() {};
-    virtual void pushBack( int c ) {};
-    virtual void popBack() {};
-    virtual void pushFront( int c ) {};
-    virtual void popFront() {};
     virtual T getHash() = 0;
 };
