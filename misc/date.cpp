7c56644baa0d9b5dd3c796e7f76e9abcd7348bbc
Hiroyuki Sano <sh19910711@gmail.com>
2013-03-18 00:21:35 +0900

    日付クラスに基本的な操作（インクリメント、文字列化など）を追加

--- misc/date.cpp
+++ misc/date.cpp
@@ -1,6 +1,44 @@
 class Date {
 public:
-    static bool isLeapYear( int y ) {
-        return y % 100 == 0 ? y % 400 == 0 : y % 4 == 0;
+    int getYear() { return yy; }
+    int getMonth() { return mm; }
+    int getDay() { return dd; }
+    void init( int yy_, int mm_, int dd_ ) { yy = yy_, mm = mm_, dd = dd_; }
+    static bool isLeapYear( int y ) { return y % 100 == 0 ? y % 400 == 0 : y % 4 == 0; }
+    virtual std::string toString() {
+        std::ostringstream oss;
+        oss << std::setw(4) << std::setfill('0') << yy << "/";
+        oss << std::setw(2) << std::setfill('0') << mm << "/";
+        oss << std::setw(2) << std::setfill('0') << dd;
+        return oss.str();
     }
+    Date& operator ++() {
+        dd ++;
+        int ld = this->days[mm] + ( mm == 2 && Date::isLeapYear(yy) ? 1 : 0 );
+        if ( dd > ld ) dd = 1, mm ++;
+        if ( mm > 12 ) mm = 1, yy ++;
+        return *this;
+    }
+    Date& operator --() {
+        dd --;
+        if ( dd == 0 ) mm --;
+        if ( mm == 0 ) mm = 12, yy --;
+        int ld = this->days[mm] + ( mm == 2 && Date::isLeapYear(yy) ? 1 : 0 );
+        dd = ld;
+        return *this;
+    }
+protected:
+    int yy, mm, dd;
+    friend bool operator <( const Date& l, const Date& r ) {
+        return l.yy == r.yy ? ( l.mm == r.mm ? ( l.dd == r.dd ? false : l.dd < r.dd ) : l.mm < r.mm ) : l.yy < r.yy;
+    }
+    friend bool operator ==( const Date& l, const Date& r ) {
+        return l.yy == r.yy && l.mm == r.mm && l.dd == r.dd;
+    }
+    friend bool operator <=( const Date& l, const Date& r ) {
+        return operator <(l, r) || operator ==(l, r);
+    }
+private:
+    static const int days[13];
 };
+const int Date::days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
