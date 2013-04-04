b191048b5e33f9786ea83f40f2418d4a385dfd0a
Hiroyuki Sano <sh19910711@gmail.com>
2013-04-05 02:04:33 +0900

    #1 メソッドの前後を1行空けた

--- solution/solution.cpp
+++ solution/solution.cpp
@@ -1,11 +1,16 @@
 // @snip <sh19910711/contest:solution/interface.cpp>
 // @snip <sh19910711/contest:solution/typedef.cpp>
+using namespace std;
+
 class Solution: public ISolution {
 public:
+
     bool input() {
     }
+
     void solve() {
     }
+
     int run() {
         while ( init(), input() ) {
             solve();
@@ -13,4 +18,5 @@ public:
         }
         return 0;
     }
+
 };
