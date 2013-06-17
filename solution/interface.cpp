d90a93542ae921e976953f9e7989d30c68261ac7
Hiroyuki Sano <sh19910711@gmail.com>
2013-06-18 04:46:39 +0900

    Fix: action(int) -> action()

--- solution/interface.cpp
+++ solution/interface.cpp
@@ -3,7 +3,7 @@ public:
   virtual int run() = 0;
 
 protected:
-  virtual bool action( int test_no ) = 0;
+  virtual bool action() = 0;
   virtual void init() {};
   virtual bool input() { return false; };
   virtual void output() {};
