b9bc60d35767fcbc8611a32fc6e6769d8c7801c5
Hiroyuki Sano <sh19910711@gmail.com>
2013-07-23 23:57:33 +0900

    #14 Add unit-tests.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 // @snip <sh19910711/contest:headers.cpp>
 // @snip <sh19910711/contest:solution/solution.cpp>
+#ifndef __MY_UNIT_TEST__
 int main() {
     return solution::Solution().run();
 }
+#endif
---
+++ unit-tests.cpp
@@ -0,0 +1,16 @@
+#define __MY_UNIT_TEST__
+#include "./main.cpp"
+#include <gtest/gtest.h>
+
+/*
+ *  # Example
+ *
+ *  int add( int x, int y ) {
+ *    return x + y;
+ *  }
+ *
+ *  TEST(AddTest, Test1) {
+ *    ASSERT_EQ(2, add(1, 1));
+ *  }
+ */
+
