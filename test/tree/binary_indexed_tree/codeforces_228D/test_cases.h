25e56c444b68e34f7e33acfcce5eade0ea536a65
Hiroyuki Sano <sh19910711@gmail.com>
2013-08-12 18:29:45 +0900

    Fix typo

--- test/tree/binary_indexed_tree/codeforces_228D/test_cases.h
+++ test/tree/binary_indexed_tree/codeforces_228D/test_cases.h
@@ -1,12 +1,12 @@
 namespace codeforces_228D {
 #include "main.cpp"
 }
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 003)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 001)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 009)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 007)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 002)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 004)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 006)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 008)
-DEFINE_INPUT_TEST(BreadthFirstSearch, codeforces_228D, 005)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 003)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 001)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 009)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 007)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 002)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 004)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 006)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 008)
+DEFINE_INPUT_TEST(BinaryIndexedTree, codeforces_228D, 005)
