49fe8eca2eee9c8b6aaf6301cacb37a6c74f8757
Hiroyuki Sano <sh19910711@gmail.com>
2013-08-16 19:30:44 +0900

    Add std::cin.sync_with_stdio(false)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // @snip <sh19910711/contest:solution/solution.cpp>
 #ifndef __MY_UNIT_TEST__
 int main() {
-    return solution::Solution().run();
+  std::cin.sync_with_stdio(false);
+  return solution::Solution().run();
 }
 #endif
