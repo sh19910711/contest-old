c5a5f5b658a4f59f78e91f2057bd7f8919a82834
Hiroyuki Sano <sh19910711@gmail.com>
2014-01-22 21:28:15 +0900

    Edit solution/define_classes

--- source/solution/define_classes.cpp
+++ source/solution/define_classes.cpp
@@ -1,2 +1,10 @@
 struct InputStorage;
 struct OutputStorage;
+struct Storages {
+  InputStorage* in;
+  OutputStorage* out;
+};
+class SolverInterface {
+public:
+  virtual const OutputStorage* solve( const InputStorage* in, OutputStorage* out ) = 0;
+};
