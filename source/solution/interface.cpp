39e3ee6304ba4a6dd63441d1a1a5f4870dff051e
Hiroyuki Sano <sh19910711@gmail.com>
2014-01-22 21:32:16 +0900

    Edit solution/interface

--- source/solution/interface.cpp
+++ source/solution/interface.cpp
@@ -1,13 +1,3 @@
-struct InputStorage;
-struct OutputStorage;
-struct Storages {
-  InputStorage* in;
-  OutputStorage* out;
-};
-class SolverInterface {
-public:
-  virtual const OutputStorage* solve( const InputStorage* in, OutputStorage* out ) = 0;
-};
 class SolutionInterface {
 public:
   virtual ~SolutionInterface() {};
